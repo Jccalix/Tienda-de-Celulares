1bdbd99cf6e6ea86b08860445c05020dff4af7b3
JC <JC@DESKTOP-BVNN4JK>
2021-11-02 17:53:46 -0600

    Cambios Antes de instalar entity framework

--- Tienda de Celulares/Tienda de Celulares/FormClientes.cs
+++ Tienda de Celulares/Tienda de Celulares/FormClientes.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Tienda_de_Celulares
 {
-    public partial class Form1 : Form
+    public partial class FormClientes : Form
     {
-        public Form1()
+        public FormClientes()
         {
             InitializeComponent();
         }
--- Tienda de Celulares/Tienda de Celulares/Program.cs
+++ Tienda de Celulares/Tienda de Celulares/Program.cs
@@ -16,7 +16,7 @@ namespace Tienda_de_Celulares
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMenu());
         }
     }
 }
